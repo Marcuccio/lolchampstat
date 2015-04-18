449cfc7ba3ec143fccd229611f313f03a57a0377
Marco <mstrambelli93@gmail.com>
2015-04-18 15:01:04 +0200

    inizi

--- js/app.js
+++ js/app.js
@@ -5,13 +5,12 @@
     app.controller('ChampController', function($scope, $http){
         $http.get('/champ_file.json').
             success(function(data, status, headers, config) {
-                $scope.posts = data;
-                 console.log("FATTO");
+                c = data;
+                console.log("FATTO");
             }).
             error(function(data, status, headers, config) {
               // log error
             });
-      
     });
     
 })();
