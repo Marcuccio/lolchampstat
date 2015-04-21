2926e268fb91ec1d5447e58d89712fdb98b998ed
Marco <mstrambelli93@gmail.com>
2015-04-21 12:42:41 +0200

    get json

--- public/app.js
+++ public/app.js
@@ -3,11 +3,11 @@
     var app = angular.module('store', [ ]);
     
     app.controller('ChampController', function($scope, $http){
-        $https.get('https://lolchampstat.herokuapp.com/champ_file.json').
+        $https.get('https://lolchampstat.herokuapp.com/champ_file.json')
             .success(function(data, status, headers, config) {
                 $scope.c = data;
                 console.log("FATTO");
-            }).
+            })
             .error(function(data, status, headers, config) {
               // log error
             });
