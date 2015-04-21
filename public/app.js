631496c8d024b7aa60a1e9ca3832b53736347631
Marco <mstrambelli93@gmail.com>
2015-04-21 12:56:46 +0200

    get json

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
     app.controller('ChampController', function($scope, $http){
         console.log("entrato nel controller");
 
-        $https.get('https://lolchampstat.herokuapp.com/champ_file.json')
+        $http.get('https://lolchampstat.herokuapp.com/champ_file.json')
             .success(function(data, status, headers, config) {
                 $scope.c = data;
                 console.log("FATTO");
