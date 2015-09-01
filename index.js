22ee257da85766dea0766e17f3e7dcca8f52d551
Marco <mstrambelli93@gmail.com>
2015-09-01 12:51:14 +0200

    static

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/img_square'));
 
 app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/img_square/*.png', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
