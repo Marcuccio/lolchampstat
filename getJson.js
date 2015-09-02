914b513be967eb1c299b7d037c5caa9c4c446eba
Marco <mstrambelli93@gmail.com>
2015-09-02 11:47:05 +0200

    added patch version

--- getJson.js
+++ getJson.js
@@ -1,9 +1,9 @@
 var https = require('https');
 var fs = require('fs');
 
-var champ = {"champions": []}; //JSON wrapper
+var champ = {"champions": [], "version":""}; //JSON wrapper
 var URLtargetListChamp = "https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=2ccc4671-a16b-461e-8ce3-8c59efec08b8";
-
+var URLversionListChamp = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/versions?api_key=2ccc4671-a16b-461e-8ce3-8c59efec08b8";
 //Multiple request, one for each champion
 function singleChamp(p) {
     for (i = 0; i < p.champions.length; i++) {
@@ -29,6 +29,20 @@ function singleChamp(p) {
     }
 } ;
 
+https.get(URLversionListChamp, function (res) {
+    var body = '';
+    res.on('data', function (data) {
+        body += data;
+    });
+    res.on('end', function () {
+        // Data reception is done, do whatever with it!
+        var parsed = JSON.parse(body);
+        champ.version=parsed[0];
+    }).on('error', function (e) {
+        console.log("Got error: " + e.message);  
+    });
+});
+
 https.get(URLtargetListChamp, function (res) {
     var body = '';
     res.on('data', function (data) {
@@ -43,3 +57,8 @@ https.get(URLtargetListChamp, function (res) {
         console.log("Got error: " + e.message);  
     });
 });
+
+
+
+
+
