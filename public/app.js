2169bb31efd01c505ee4af6ca593ed2fa96a2a75
Marcuccio <MStrambelli93@gmail.com>
2015-10-30 23:29:06 +0100

    compile

--- public/app.js
+++ public/app.js
@@ -1,6 +1,31 @@
 (function(){
     
-	var app = angular.module('stats', ['ngSanitize']);
+	var app = angular.module('stats', ['ngSanitize']){
+    // configure new 'compile' directive by passing a directive
+    // factory function. The factory function injects the '$compile'
+    $compileProvider.directive('compile', function($compile) {
+      // directive factory creates a link function
+      return function(scope, element, attrs) {
+        scope.$watch(
+          function(scope) {
+             // watch the 'compile' expression for changes
+            return scope.$eval(attrs.compile);
+          },
+          function(value) {
+            // when the 'compile' expression changes
+            // assign it into the current DOM
+            element.html(value);
+
+            // compile the new DOM and link it to the current
+            // scope.
+            // NOTE: we only compile .childNodes so that
+            // we don't get into infinite loop compiling ourselves
+            $compile(element.contents())(scope);
+          }
+        );
+      };
+    });
+  };
 
 	app.controller('ChampController', function($scope, $http){
 		$scope.predicate = 'name';
@@ -14,16 +39,19 @@
 			})
 	});
 		
-	app.controller('ModalController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+	app.controller('ModalController', ['$scope', '$http', function($scope, $http){
 		this.loadModal = function(id){
 			$http.get('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/'+id+'?locale=it_IT&champData=passive,skins,spells&api_key=2ccc4671-a16b-461e-8ce3-8c59efec08b8')
 			.success(function(data, status, headers, config) {
 				 $scope.m = data;
 			});
 			
-			$scope.trustSnippet = function(snippet) {
-               return $sce.trustAsHtml(snippet);
-			};
+			$scope.passive=m.passive.sanitizedDescription;
+			$scope.Q=m.spells[0].tooltip;
+			$scope.W=m.spells[1].tooltip;
+			$scope.E=m.spells[2].tooltip;
+			$scope.R=m.spells[3].tooltip;
+			
 		}
 	}]);
 	
